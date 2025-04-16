e1ac78e6076fcbc7c9fb771b62ada27220372ffb
Thorffinth <thorffinth@gmail.com>
2025-04-15 23:17:18 -0600

    Add files via upload

---
+++ calculadorav_0.1.py
@@ -0,0 +1,35 @@
+#esto seria algo muy simple y basico, solo muestra resultado y para editar los numeros tendrias que hacerlo tu mismo cambiando unas partes del codigo 
+print ("calculadora basica ")
+#aqui puedes editarlo si quieres moverle algo crack
+print ("suma de 3 digitos")
+num1 = 20 #puedes editarlo si gustas como dije anteriormente.
+num2 = 20 
+num3 = 10
+resultado = num1 + num2 + num3
+print ("el resultado al sumar es:", resultado)
+
+print("--------------------------------------------------------")
+
+print("multiplicacion de 3 y 2 digitos :v ")
+mul1 = 2
+mul2 = 7
+mul3 = 5
+resultado = 2 * 7 * 5
+print ("El resultado de tu miserable vida es de:", resultado)
+
+#multiplicacion de 2 digitos
+print ("multiplicacion de 2 digitos")
+mul1 = 7
+mul2 = 3 #aqui creo que tal vez estoy mal, nose si deberia poner esto o repetirlo :v
+resultado = mul1 * mul2
+print ("el resultado de tu operacion es:", resultado)
+
+print("--------------------------------------------------------")
+
+print ("diviciones de 2 digitos ")
+div1 = 2
+div2 = 12 #recuerda que esto es basico y puedes editar esto si quieres :D
+resultado = div1 / div2
+print (" tu resultado de tu operacion es:", resultado)
+
+#Esto seria todo, algo basico y tal vez ayude a alguien
